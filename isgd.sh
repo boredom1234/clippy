3289a1a22e0b17283595bd61389b73ae5699d8ab
boredom1234 <90498974+boredom1234@users.noreply.github.com>
2024-02-03 10:43:37 +0530

    Update isgd.sh

--- isgd.sh
+++ isgd.sh
@@ -7,7 +7,7 @@ REMOTE_PORT="80"
 # Your existing SSH command
 SSH_COMMAND="ssh -R $REMOTE_PORT:localhost:$LOCAL_PORT"
 
-# Shorten the URL using is.gd
-shortened_url=$(curl -s "https://is.gd/create.php?format=simple" --data-urlencode "url=$SSH_COMMAND")
+# Shorten the URL using TinyURL
+shortened_url=$(curl -s "http://tinyurl.com/api-create.php?url=$SSH_COMMAND")
 
 echo "Shortened URL: $shortened_url"
